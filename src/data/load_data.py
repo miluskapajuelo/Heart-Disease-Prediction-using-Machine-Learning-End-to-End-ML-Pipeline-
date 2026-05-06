b6bd7c460f9051fc09eaeb6d8b990815df4a39f1
miluskapajuelo <miluskapajuelo@gmail.com>
2026-05-06 17:12:09 -0400

    handle errors importing data

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,13 +1,45 @@
 from pathlib import Path
 import pandas as pd
+from src.utils.config import get_path
+from src.utils.helpers import check_duplicates
 
 
-def load_data(file_path:str | Path) -> pd.DataFrame:
+
+def load_data() -> pd.DataFrame:
     """
-    Load a dataset from a CVS file.
+    Load a dataset from the heart disease CSV file.
+
+    Returns: 
+        df: A pandas Dataframe containing the loaded data.
+
+    """
+    path = get_path("raw_data")
+    if not path.exists():
+        raise FileNotFoundError(
+            f"Raw data not found at {path}"
+           )
+
+    if path.suffix.lower() != ".csv":
+        raise ValueError(
+            f"Expected a .csv file, got {path.suffix}"
+        )
+    
+    df = pd.read_csv(path)
+
+    if df.empty:
+        raise ValueError(
+            f"File loaded but contains no rows {path}"
+        )
+
+    return df
+
+
+def drop_duplicated(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Remove duplicated rows from dataset.
 
     Args: 
-        file_path: Path to the CSV file.
+        df: dataframe from .csv.
 
     Returns: 
         A pandas Dataframe containing the loaded data.
@@ -16,16 +48,9 @@ def load_data(file_path:str | Path) -> pd.DataFrame:
         FileNotFoundError: if the file does not exist.
         ValueError: if the loaded dataframe is empty.
     """
-    file_path = Path(file_path)
-
-    if not file_path.exists():
-        raise FileNotFoundError(f"File not found: {file_path}")
-    
-    df = pd.read_csv(file_path)
-
-    if df.empty:
-        raise ValueError(f"The file is empty: {file_path}")
-    
-    return df
-
+   
+    duplicates = check_duplicates(df)
+    if duplicates:
+        df.drop_duplicates()
+        return df
 
